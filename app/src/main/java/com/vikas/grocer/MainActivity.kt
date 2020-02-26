94d71bf09599ce6cbc511f89f834ca31ef1aed8a
Vikas kumar <vkbookworm@gmail.com>
2020-02-26 23:59:25 +0530

    feat: added the custom toolbar.xml

--- app/src/main/java/com/vikas/grocer/MainActivity.kt
+++ app/src/main/java/com/vikas/grocer/MainActivity.kt
@@ -2,9 +2,15 @@ package com.vikas.grocer
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.vikas.grocer.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
+    private lateinit var mainActivityMainBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        mainActivityMainBinding = ActivityMainBinding.bind(window.decorView.rootView)
     }
+
 }
